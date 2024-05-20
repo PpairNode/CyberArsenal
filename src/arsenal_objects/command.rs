72cb744eafe85dd40e4bebc8b1bfce5a65ceaa90
PpairNode <user@user.user>
2024-05-20 04:17:59 -0400

    [ARG] add: id to CommandArg

--- src/arsenal_objects/command.rs
+++ src/arsenal_objects/command.rs
@@ -34,34 +34,35 @@ impl CommandType {
 #[derive(Debug, Clone)]
 // Structure used to represent modifications on a command arg
 pub struct CommandArg {
-    value: String,              // Litteral value, e.g. '<port=4444>'. This value is always set up.
-    is_input: bool,             // If this value has to be an input
-    default: Option<String>,    // If value is '<port=4444>' then default would be 4444. This would be the second value to be taken if not empty.
+    id: usize,                      // Used to know which argument is being modified
+    value: String,                  // Litteral value, e.g. '<port=4444>'. This value is always set up.
+    is_input: bool,                 // If this value has to be an input
+    default: Option<String>,        // If value is '<port=4444>' then default would be 4444. This would be the second value to be taken if not empty.
     pub modified: Option<String>,   // If value is overriden by user input then it is modified here. This would be the first value to be taken if not empty.
 }
 
 impl CommandArg {
-    pub fn new(arg: String) -> CommandArg {
-        let mut arg_fill = CommandArg { value: arg.clone(), is_input: false, default: None, modified: None };
+    pub fn new(id: usize, arg: String) -> CommandArg {
+        let mut cmd_arg = CommandArg { id, value: arg.clone(), is_input: false, default: None, modified: None };
 
         let re = match Regex::new(r"<([a-zA-Z0-9=-_.]+)>") {
             Ok(r) => r,
             Err(_) => {
-                return arg_fill
+                return cmd_arg
             }
         };
 
         for (_, [cap]) in re.captures_iter(&arg).map(|c| c.extract()) {
             let s = cap.split("=").map(|s| s.to_string()).collect::<Vec<String>>();
             if s.len() == 2 {  // Value default set
-                arg_fill.value = format!("<{}>", s.get(0).unwrap().clone());
-                arg_fill.default = Some(s.get(1).unwrap().clone());
+                cmd_arg.value = format!("<{}>", s.get(0).unwrap().clone());
+                cmd_arg.default = Some(s.get(1).unwrap().clone());
             } else if s.len() == 1 {
-                arg_fill.value = format!("<{}>", s.get(0).unwrap().clone());
+                cmd_arg.value = format!("<{}>", s.get(0).unwrap().clone());
             }
-            arg_fill.is_input = true;
+            cmd_arg.is_input = true;
         }
-        arg_fill
+        cmd_arg
     }
 
     pub fn copy(self) -> String {
@@ -84,11 +85,11 @@ impl Display for CommandArg {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         if self.is_input {
             if self.modified.is_some() {
-                return write!(f, "{} = {}", self.value, self.modified.clone().unwrap())
+                return write!(f, "({}){} = {}", self.id, self.value, self.modified.clone().unwrap())
             } else if self.default.is_some() {
-                return write!(f, "{} = {}", self.value, self.default.clone().unwrap())
+                return write!(f, "({}){} = {}", self.id, self.value, self.default.clone().unwrap())
             } else {
-                return write!(f, "{} = ", self.value);
+                return write!(f, "({}){} = ", self.id, self.value);
             }    
         }
 
@@ -112,9 +113,14 @@ impl Command {
         static mut ID: usize = 0;
         unsafe { ID = ID + 1 };
 
+        let mut id = 0;
         let v = args.split_whitespace().map(|s| s.to_string()).collect::<Vec<String>>();
         let cmd_args: Vec<CommandArg> = v.iter()
-            .map(|s| CommandArg::new(s.to_string()))
+            .map(|s| {
+                let cmd_arg = CommandArg::new(id, s.to_string());
+                id += 1;
+                cmd_arg
+            })
             .collect();
 
         let cmd_type = CommandType::from_str(&cmd_type);
